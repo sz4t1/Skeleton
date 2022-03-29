cba897256bf6128f80ccfb83cdb2789553d78ba1
Nagy Gergő <79868004+nagygergo314@users.noreply.github.com>
2022-03-29 16:42:18 +0200

    CompleteProtection

--- CompleteProtection.java
+++ CompleteProtection.java
@@ -1,6 +1,9 @@
 //This is an ability which the player can have, this blocks the incoming attack
 //This class extends the ProtectionAbility abstract class
 public class CompleteProtection extends ProtectionAbility {
+    public CompleteProtection(){
+        System.out.println("CompleteProtection() - CompleteProtection (ability) constructed.");
+    }
     //We override the method here and it gives the Virologist the ability to not get hit by the incoming ability
     @Override
     public void invokeEffect(Virologist v) {
