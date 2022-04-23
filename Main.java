e725ab6f86b57bce6b3017ee996e6bc6fa3ccd1c
Nagy Gergő <79868004+nagygergo314@users.noreply.github.com>
2022-04-23 04:38:51 +0200

    scanner hiba debug
    NoSuchElemnt hiba: valszeg mert egyszerre próbál a scanner és a bufferedreader is a standard bemenethez hozzáférni

--- Main.java
+++ Main.java
@@ -5,19 +5,89 @@ public class Main {
         Game game = new Game();
         
         Integer input=0;
+        Integer i = 0;
         Scanner sc = new Scanner(System.in);
 
         while(input!=-1){
             Menu.menuElements();
             input=sc.nextInt();
-
+                    
             switch(input){
                 case 0:{
                     sc.close();
                     System.exit(0); break;
                 }
                 case 1:{
-                    testMenu();
+                    while(i != -1){
+                        Menu.testMenuElements();
+                        i=sc.nextInt();
+                        switch(i){
+                            case 0:{
+                                i = -1; break;
+                            }
+                            case 1:{
+                                
+                                break;
+                            }
+                            case 2:{
+                                
+                                break;
+                            }
+                            case 3:{
+                                
+                                break;
+                            }
+                            case 4:{
+                                
+                                break;
+                            }
+                            case 5:{
+                                
+                                break;
+                            }
+                            case 6:{
+                                
+                                break;
+                            }
+                            case 7:{
+                                
+                                break;
+                            }
+                            case 8:{
+                                
+                                break;
+                            }
+                            case 9:{
+                                
+                                break;
+                            }
+                            case 10:{
+                               
+                                break;
+                            }
+                            case 11:{
+                               
+                                break;
+                            }
+                            case 12:{
+                                
+                                break;
+                            }
+                            case 13:{
+                                
+                                break;
+                            }
+                            case 14:{
+                                
+                                break;
+                            }
+                            case 15:{
+                                
+                                break;
+                            }
+                            default:{}
+                        }
+                    }
                     break;
                 }
                 case 2:{
@@ -32,83 +102,7 @@ public class Main {
 
                     break;
                 }
-            }
-        }
-    }
-
-    public static void testMenu(){
-        Integer input=0;
-        Scanner scm = new Scanner(System.in);
-
-        while(input!=-1){
-            Menu.testMenuElements();
-            input=scm.nextInt();
-
-            switch(input){
-                case 0:{
-                    scm.close();
-                    input = -1; break;
-                }
-                case 1:{
-                    
-                    break;
-                }
-                case 2:{
-                    
-                    break;
-                }
-                case 3:{
-                    
-                    break;
-                }
-                case 4:{
-                    
-                    break;
-                }
-                case 5:{
-                    
-                    break;
-                }
-                case 6:{
-                    
-                    break;
-                }
-                case 7:{
-                    
-                    break;
-                }
-                case 8:{
-                    
-                    break;
-                }
-                case 9:{
-                    
-                    break;
-                }
-                case 10:{
-                   
-                    break;
-                }
-                case 11:{
-                   
-                    break;
-                }
-                case 12:{
-                    
-                    break;
-                }
-                case 13:{
-                    
-                    break;
-                }
-                case 14:{
-                    
-                    break;
-                }
-                case 15:{
-                    
-                    break;
-                }
+                default:{}
             }
         }
     }
