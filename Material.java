11231d34116c1a4a2bd8d9cc4dc1a6b984f45d33
Nagy Gergő <79868004+nagygergo314@users.noreply.github.com>
2022-05-17 20:21:27 +0200

    materials name

--- Material.java
+++ Material.java
@@ -1,15 +1,11 @@
 //Abstract class that represents the materials that can be used to create viruses
 public abstract class Material {
     //Id of the material
-    private String Name;
+    protected String name;
     //Method that deletes the material after it was used
     public abstract void beingUsed();
     //Get-set
     public String getName() {
-        return Name;
-    }
-
-    public void setName(String name) {
-        Name = name;
+        return name;
     }
 }
