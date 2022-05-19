9709fd8a6269cc623cdeb6d1c1cbad975badc0a1
sz4t1 <71267226+sz4t1@users.noreply.github.com>
2022-05-19 17:07:37 +0200

    Add files via upload
    Elvileg jó?! :=(

--- DrawField.java
+++ DrawField.java
@@ -11,39 +11,39 @@ public class DrawField extends JLabel {
         setBackground(Color.lightGray);
     }
     public void paintComponent(Graphics g){
-        /*Color[] colors= new Color[4];
-        int colorNums=field.getVirologists.getSize();
+        Color[] colors= new Color[4];
+        int colorNums=field.getVirologists().size();
         for(int i =0; i<colorNums ;i++)
-            switch(field.getVirologists.get(i).getName){
+            switch(field.getVirologists().get(i).GetName()){
                 case "Virologist1":{ colors[i]=Color.blue; break;}
                 case "Virologist2":{ colors[i]=Color.ORANGE; break;}
                 case "Virologist3":{ colors[i]=Color.CYAN; break;}
                 case "Virologist4":{ colors[i]=Color.PINK; break;}
             }
-            */
+
         super.paintComponent(g);
-        if(/*field.getVirologists.getsize()*/x>=1) {
+        if(field.getVirologists().size()>=1) {
             g.setColor(Color.black);
             g.drawOval(24, 72, 20, 20);
-            g.setColor(/*colors[0]*/Color.blue);
+            g.setColor(colors[0]);
             g.fillOval(24, 72, 20, 20);
         }
-        if(/*field.getVirologists.getsize()*/x>=2) {
+        if(field.getVirologists().size()>=2) {
             g.setColor(Color.black);
             g.drawOval(24, 24, 20, 20);
-            g.setColor(/*colors[1]*/Color.ORANGE);
+            g.setColor(colors[1]);
             g.fillOval(24, 24, 20, 20);
         }
-        if(/*field.getVirologists.getsize()*/x>=3) {
+        if(field.getVirologists().size()>=3) {
             g.setColor(Color.black);
             g.drawOval(72, 24, 20, 20);
-            g.setColor(/*colors[2]*/Color.CYAN);
+            g.setColor(colors[2]);
             g.fillOval(72, 24, 20, 20);
         }
-        if(/*field.getVirologists.getsize()*/x==4) {
+        if(field.getVirologists().size()==4) {
             g.setColor(Color.black);
             g.drawOval(72, 72, 20, 20);
-            g.setColor(/*colors[3]*/Color.PINK);
+            g.setColor(colors[3]);
             g.fillOval(72, 72, 20, 20);
         }
     }
